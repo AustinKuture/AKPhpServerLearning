93b817f43a54d402e217942721c8df675d22db20
AustinKuture <ya_kun_13@163.com>
2017-06-02 09:01:52 +0800

    add folder

---
+++ Class/Phplearning00.php
@@ -0,0 +1,21 @@
+<?php
+
+//$mutiArray = array(
+//
+//    array("32","kture","e2d"),
+//    array("aus","find",23),
+//    array("yikun",89,"work",909,"workfollow")
+//);
+//
+//var_dump($mutiArray);
+//echo "<br>--------------------------------<br>";
+//print_r($mutiArray);
+
+$mutiArrayTwo = array(
+
+  "Kuture" => array("findSomething",889,"what","enen"),
+    "Austin" => array("NewClearWeapon","009","3"),
+    90 => array("I am here","what the fox","greatCow","what ?"),
+);
+
+print_r($mutiArrayTwo['Kuture'][0]);
