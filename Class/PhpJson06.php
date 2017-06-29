435e6501ebc78f3975eb662f8ce371a8685b9046
李亚坤 <austinkuture@126.com>
2017-06-29 13:44:44 +0800

    加入Json解析

---
+++ Class/PhpJson06.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kuture
+ * Date: 2017/6/29
+ * Time: 13:23
+ */
+
+   $userArr = array('name' => 'Kuture','age' => '18','heigh' => '175','weight' => '60Kg');
+
+   echo "<br>".var_dump($userArr)."<br>"."<br>".json_encode($userArr)."<br>";
+
+?>
